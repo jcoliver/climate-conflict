01d0dcb63af2a3702aa3ca58f0938f79168ba962
Jeff Oliver <jcoliver@email.arizona.edu>
2023-05-19 14:11:36 -0700

    Change to GADM for municipality boundaries

--- create-muni-weather.R
+++ create-muni-weather.R
@@ -4,16 +4,15 @@
 # 2023-05-19
 
 # Load libraries
-library(sf)    # reading in shapefiles
-library(terra) # raster functions
-library(dplyr) # data wrangling
+library(geodata) # downloading municipality administrative boundaries
+library(sf)      # reading in shapefiles
+library(terra)   # raster functions
+library(dplyr)   # data wrangling
 
-# Shapefile data for municipality boundaries are from geoBoundaries
-# https://www.geoboundaries.org/index.html#getdata
-# Use geoBounaries
-# Subnational Administrative Boundaries level 2 (ADM2) are municipalities
-# municipality names are in the shapeName field, polygon is in the geometry
-# field
+# Municipality bounaries are from GADM, https://gadm.org
+# NAME_1 = department (ADM1)
+# NAME_2 = municipality (ADM2)
+# Retrieved (if necessary) via geodata::gadm()
 
 # Create a single data frame with the following columns:
 # country | municipality | temp | prec_gpcp | droughtcrop_speibase | year
@@ -22,19 +21,26 @@ library(dplyr) # data wrangling
 # Four countries: El Salvador, Guatemala, Honduras, Mexico
 
 # Make a data frame for easier iteration and file path information
-shapefile_info <- data.frame(dir_name = c("el-salvador", "guatemala", 
+boundaries_info <- data.frame(dir_name = c("el-salvador", "guatemala", 
                                           "honduras", "mexico"),
                              ctry_code = c("SLV", "GTM", "HND", "MEX"))
+
+# Check for boundary data files; download them if they do not exist locally
+for (row_i in 1:nrow(boundaries_info)) {
+  ctry_code <- boundaries_info$ctry_code[row_i]
+  if (!file.exists(paste0("data/gadm/", tolower(ctry_code), "/gadm41_",
+                          ctry_code, "_2_pk.rds"))) {
+    message("Downloading ADM2 data for ", ctry_code)
+    ctry_data <- geodata::gadm(country = ctry_code,
+                               level = 2,
+                               path = paste0("data/gadm/", tolower(ctry_code)))
+  }
+}
+
 # Make a column with file path information
-# Could use the simplified shapes, instead. Resolution is lower, but not by 
-# much
-shapefile_info <- shapefile_info %>%
-  mutate(filename = paste0("data/municipality-shapefiles/", dir_name, 
-                           "/geoBoundaries-", ctry_code, "-ADM2.shp"))
-# Simplified version
-# shapefile_info <- shapefile_info %>%
-#   mutate(filename = paste0("data/municipality-shapefiles/", dir_name, 
-#                            "/geoBoundaries-", ctry_code, "-ADM2_simplified.shp"))
+boundaries_info <- boundaries_info %>%
+  mutate(filename = paste0("data/gadm/", tolower(ctry_code), "/gadm41_",
+                           ctry_code, "_2_pk.rds"))
 
 # Load in weather data
 load(file = "data/grid_combined_4country.RData")
@@ -43,19 +49,15 @@ load(file = "data/grid_combined_4country.RData")
 years <- sort(unique(grid_combined_4country$year))
 
 # List to hold data, one element per country, indexed by directory name
-all_countries_list <- vector(mode = "list", length = nrow(shapefile_info))
-names(all_countries_list) <- shapefile_info$dir_name
+all_countries_list <- vector(mode = "list", length = nrow(boundaries_info))
+names(all_countries_list) <- boundaries_info$dir_name
 
 # Takes a couple of minutes, mostly as it works on the data for Mexico
-for (row_i in 1:nrow(shapefile_info)) {
-  dir_name <- shapefile_info$dir_name[row_i]
+for (row_i in 1:nrow(boundaries_info)) {
+  dir_name <- boundaries_info$dir_name[row_i]
   message("Processing weather for directory ", dir_name)
-  # Read in the shapefile of municipality boundaries for the country of interest
-  muni_shapes <- sf::st_read(dsn = shapefile_info$filename[row_i], 
-                             quiet = TRUE)
-  
-  # Turn this shape into a SpatVector
-  muni_vectors <- terra::vect(muni_shapes)
+  # Read in the municipality boundaries for the country of interest
+  muni_vectors <- readRDS(file = boundaries_info$filename[row_i])
   
   # Use vectorization to apply to all years, ignoring Demeter a bit
   muni_data_list <- base::lapply(X = years, 
@@ -75,16 +77,18 @@ for (row_i in 1:nrow(shapefile_info)) {
                              mean, 
                              na.rm = TRUE)
      # Add municipality names to this weather data frame; have to wrap in a call 
-     # to list() with named element for the municipality; otherwise throws 
-     # annoying "New names" message and requires subsequent assignment for the 
-     # column where shapeName data end up. Also, 
+     # to list() with named elements for the municipality and department; 
+     # otherwise throws annoying "New names" message and requires subsequent 
+     # assignment for the column where shapeName data end up. Also, 
      #   + drop the manufactured ID column
      #   + add a column for the current year
-     muni_data <- dplyr::bind_cols(list(municipality = muni_vectors$shapeName, 
+     muni_data <- dplyr::bind_cols(list(municipality = muni_vectors$NAME_2, 
+                                        department = muni_vectors$NAME_1,
                                         muni_weather)) %>%
        dplyr::select(-ID) %>%
-       dplyr::mutate(year = year_i) %>%
-       return(muni_data)
+       dplyr::mutate(year = year_i)
+     
+     return(muni_data)
    })
   # Bind data for all years into a single data frame
   all_countries_list[[dir_name]] <- dplyr::bind_rows(muni_data_list)
--- join-muni-data.R
+++ join-muni-data.R
@@ -12,30 +12,39 @@ muni_weather <- read.csv(file = "data/muni-weather.csv")
 # Load in flooding data
 load(file = "data/flooding_mun_4country.RData")
 
-# Pull out just columns of interest from the flood dataset
+# Pull out just columns of interest from the flood dataset; needs some 
+# de-duplication, so we start with larger sampling of columns
 flood_data <- flooding_mun_4country %>%
-  select(Country, municipality, year, month)
+  mutate(muni_lower = tolower(municipality)) %>%
+  select(-c(department, munID_str, id, municipality)) %>%
+  distinct() %>%
+  rename(country = Country) %>%
+  select(country, muni_lower, year, month)
 
-# Summarize the flood data by country, municipality, year, and month
+# Create muni_lower column for easier joining
+muni_weather <- muni_weather %>%
+  mutate(muni_lower = tolower(municipality))
+
+# Summarize the flood data by country, municipality, year, and month; prepare 
+# for join with weather data
 flood_summary <- flood_data %>%
-  group_by(Country, municipality, year, month) %>%
+  group_by(country, muni_lower, year, month) %>%
   summarize(flood_count = n()) %>%
   ungroup() %>%
   mutate(year = as.numeric(year),
          month = as.numeric(month))
 
-# Join the weather data by municipality & year; start by creating dummy column 
-# muni_lower in each dataset for easier joining; remove original municipality 
-# column from from flood data before join
-muni_weather <- muni_weather %>%
-  mutate(muni_lower = tolower(municipality))
-flood_summary <- flood_summary %>%
-  mutate(muni_lower = tolower(municipality)) %>%
-  select(-municipality)
-
-# TODO: getting many-to-many warning here that should be addressed
-muni_combined <- flood_summary %>%
-  full_join(muni_weather, by = c("Country" = "country", "muni_lower", "year"))
+head(flood_summary)
+head(muni_weather)
+# TODO: getting many-to-many warning here that should be addressed; one case is 
+# in the weather data; should really only have one row per country/muni/year in 
+# that data set...
+# country  municipality     temp prec_gpcp droughtcrop_speibase year    muni_lower
+# 1 El Salvador Santo Domingo 26.59833  1650.678           0.00000000 1992 santo domingo
+# 2 El Salvador Santo Domingo 25.06667  1650.678           0.08333334 1992 santo domingo
+muni_combined <- muni_weather %>%
+  full_join(flood_summary, by = c("country", "muni_lower", "year")) %>%
+  select(-muni_lower)
 
 head(muni_weather)
 
